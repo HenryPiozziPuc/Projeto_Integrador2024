eb44fa46369def21dd57842a42ecdea7502ff83c
Henry Piozzi <henry.gp@puccampinas.edu.br>
2024-03-26 21:28:36 -0300

    Um teste colocando o registro e calculo juntos

--- Códigos Python/Cadastron.py
+++ Códigos Python/Cadastron.py
@@ -9,12 +9,11 @@ def obtenção_de_dados():
     comissao_vendas = float(input("Digite a comissão de vendas:\n "))
     imposto_prod =float(input("Digite o imposto sobre a venda:\n "))
     lucro_prod =float(input("Digite a margem de lucro:\n "))
-    return cod_prod, nome_prod, desc_prod, custo_prod, custo_fixo_prod, comissao_vendas, imposto_prod, lucro_prod
 
-def calculo_preco(custo_prod, custo_fixo_prod, comissao_vendas, imposto_prod, lucro_prod):
     preco_venda = (custo_prod)/(1-((custo_fixo_prod + comissao_vendas + imposto_prod + lucro_prod)/100))
     custos = preco_venda*((custo_fixo_prod+comissao_vendas+imposto_prod)/100)
     rentabilidade = ((preco_venda/custo_prod) - 1)*100
+
     print(f"O preço de venda do produto é R${preco_venda:.2f}, o lucro bruto é {rentabilidade:.2f}% e os custos são de {custos:.2f}")
     if rentabilidade>=20:
         print("\nO lucro foi alto")
@@ -24,7 +23,7 @@ def calculo_preco(custo_prod, custo_fixo_prod, comissao_vendas, imposto_prod, lu
         print("\nO lucro foi baixo")
     else:
         print("\nFicou no prejuízo")
-    return preco_venda, custos, rentabilidade
+    return cod_prod, nome_prod, desc_prod, custo_prod, custo_fixo_prod, comissao_vendas, imposto_prod, lucro_prod, preco_venda, custos, rentabilidade
 
 def main():
     escolha = input("Qual ação deseja realizar?\nRegistrar |Listar    |Alterar   |Excluir  |\n")
