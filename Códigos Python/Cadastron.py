bf328592135f72f722ba57991574c52d2f4fa2aa
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-03-27 14:15:52 -0300

    Implementação table

--- Códigos Python/Cadastron.py
+++ Códigos Python/Cadastron.py
@@ -1,23 +1,50 @@
 import os
+from prettytable import PrettyTable
 
 # Definição da função para cadastrar um novo produto
 def cadastrar_produto():
     os.system("cls")  # Limpa a tela do console (apenas Windows)
+    table = PrettyTable()
+
     print(f"\n\tCadastro de produto!")  # Mensagem de cabeçalho para o cadastro
     # Solicita e armazena os dados do produto inseridos pelo usuário
+    global cod_prod
     cod_prod = int(input("Código do produto: "))
-    nome_prod = input("Nome do produto: ")
-    desc_prod = input("Descrição: ")
-    custo_prod = float(input("Custo de compra: R$ "))
-    custo_fixo_prod = float(input("Custo fixo/administrativo: "))
-    comissao_vendas = float(input("Comissão de vendas: "))
-    imposto_prod = float(input("Imposto sobre a venda: "))
-    lucro_prod = float(input("Margem de lucro em porcentagem: "))
-    # Retorna os dados do produto
-    return cod_prod, nome_prod, desc_prod, custo_prod, custo_fixo_prod, comissao_vendas, imposto_prod, lucro_prod
+    table.add_column(f"Código do Produto",[cod_prod])
 
+    global nome_prod
+    nome_prod = input("Nome do produto: ")
+    table.add_column(f"Nome",[nome_prod])
+
+    global desc_prod
+    desc_prod = input("Descrição: ")
+    table.add_column(f"Descrição",[desc_prod])
+
+    global custo_prod
+    custo_prod = float(input("Custo de compra: R$ "))
+    table.add_column(f"Custo de Compra",[custo_prod])
+
+    global custo_fixo_prod
+    custo_fixo_prod = float(input("Custo fixo/administrativo: "))
+    table.add_column(f"Custo Fixo/Administrativo",[custo_fixo_prod])
+
+    global comissao_vendas
+    comissao_vendas = float(input("Comissão de vendas: "))
+    table.add_column(f"Comissão de vendas",[comissao_vendas])
+
+    global imposto_prod
+    imposto_prod = float(input("Imposto sobre a venda: "))
+    table.add_column(f"Imposto sobre a venda",[imposto_prod])
+
+    global lucro_prod
+    lucro_prod = float(input("Margem de lucro em porcentagem: "))
+    table.add_column(f"Imposto sobre a venda",[lucro_prod])
+    # Retorna os dados do produto
+    print(f"\n{table}")
+    calculo_preco()
+    
 # Definição da função para calcular o preço de venda, custos e rentabilidade do produto
-def calculo_preco(custo_prod, custo_fixo_prod, comissao_vendas, imposto_prod, lucro_prod):
+def calculo_preco():
     # Cálculo do preço de venda
     preco_venda = (custo_prod) / (1 - ((custo_fixo_prod + comissao_vendas + imposto_prod + lucro_prod) / 100))
     # Cálculo dos custos
